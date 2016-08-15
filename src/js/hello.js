b7db753d17d87715e7a9c936b470efdafdc4287c
Kalabasa <godffrey0@gmail.com>
2016-08-16 00:02:06 +0800

    Update new info.

--- src/js/hello.js
+++ src/js/hello.js
@@ -1,4 +1,26 @@
 "use strict";
-(function($){
+(function(_,$){
 	var $window = $(window);
-})(jQuery);
+	var $hello_me_title = $("#hello-me-title");
+
+	const interval = 4000;
+	var titles = window.bio.titles;
+
+	var next_title_index = 0;
+	var next_titles = _.shuffle(titles.slice(1));
+
+	var change_title = function(new_title) {
+		$hello_me_title.fadeOut(60, function() {
+			$hello_me_title.text(new_title);
+			$hello_me_title.fadeIn(150);
+		});
+	}
+
+	setInterval(function() {
+		change_title(next_titles[next_title_index++]);
+		if (next_title_index >= next_titles.length) {
+			next_title_index = 0;
+			next_titles = _.shuffle(titles);
+		}
+	}, interval);
+})(_,jQuery);
