41214cdbabc56ee743e5ccdacd171e501302a61a
Lean Rada <godffrey0@gmail.com>
2017-02-15 01:32:26 +0800

    Deploy

---
+++ build/js/bower-requirejs-config.js
@@ -0,0 +1,15 @@
+require.config({
+  shim: {
+
+  },
+  paths: {
+    jquery: "../jquery/dist/jquery",
+    "jquery-bez": "../jquery-bez/jquery.bez.min",
+    requirejs: "../requirejs/require",
+    threejs: "../threejs/rollup.config",
+    underscore: "../underscore/underscore"
+  },
+  packages: [
+
+  ]
+});
