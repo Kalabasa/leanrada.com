23941c8f4cb18393deb2b10f06c580eb252c0563
Lean Rada <godffrey0@gmail.com>
2018-07-16 02:15:04 +0800

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,23 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+git add -f build/
+STASH=(git stash create)
+
 git checkout -B deploy
 git merge --no-edit -X theirs src
+
 yarn install
 rm -rf build/
 yarn build --prod
+
 git add -f build/
 git commit -m "Deploy"
+
 git branch -D master
 git subtree split -P build/ -b master
+
 git push -f origin master:master
+
 git checkout @{-1}
+if [ -n "${STASH}" ] ; then
+	git stash pop
+fi
