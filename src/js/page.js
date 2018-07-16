b5d1912e8388106afebc1dc9d59228fa58e5b7e9
Lean Rada <godffrey0@gmail.com>
2018-07-16 16:46:24 +0800

    Fix scroll issue in page transition

--- src/js/page.js
+++ src/js/page.js
@@ -28,7 +28,6 @@ function onReady() {
 }
 
 function onLoad() {
-	Barba.Pjax.preventCheck = function() { return preventCheck.apply(this, arguments) };
 	Barba.Pjax.getTransition = function() { return transition };
 
 	Barba.Pjax.start();
@@ -108,49 +107,6 @@ const transition = Barba.BaseTransition.extend({
 function getPageName() {
 	if (window.location.pathname.endsWith('/')) return 'index.html';
 	return window.location.pathname.split('/').slice(-1)[0];
-}
-
-// Modified from barba.js 965a266 to allow hash on different page
-function preventCheck(evt, element) {
-    if (!window.history.pushState)
-      return false;
-
-    var href = this.getHref(element);
-
-    //User
-    if (!element || !href)
-      return false;
-
-    //Middle click, cmd click, and ctrl click
-    if (evt.which > 1 || evt.metaKey || evt.ctrlKey || evt.shiftKey || evt.altKey)
-      return false;
-
-    //Ignore target with _blank target
-    if (element.target && element.target === '_blank')
-      return false;
-
-    //Check if it's the same domain
-    if (window.location.protocol !== element.protocol || window.location.hostname !== element.hostname)
-      return false;
-
-    //Check if the port is the same
-    if (Barba.Utils.getPort() !== Barba.Utils.getPort(element.port))
-      return false;
-
-    // REMOVED HASH CHECK
-
-    //Ignore case where there is download attribute
-    if (element.getAttribute && typeof element.getAttribute('download') === 'string')
-      return false;
-
-    //In case you're trying to load the same page
-    if (Barba.Utils.cleanLink(href) == Barba.Utils.cleanLink(location.href))
-      return false;
-
-    if (element.classList.contains(this.ignoreClassLink))
-      return false;
-
-    return true;
 }
 
 export default {
