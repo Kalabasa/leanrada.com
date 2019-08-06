11673ca3601532006785048a6bcf3ecfd93ea6ec
Kalabasa <Kalabasa@users.noreply.github.com>
2019-08-06 09:41:36 +0800

    Add Dimensions article

--- src/data/data.js
+++ src/data/data.js
@@ -2,8 +2,18 @@ import _ from 'lodash';
 
 // data for the whole site!
 export default {
-	featured_projects: [ 'canvaphotoeditor', 'hypertangram', 'planetdefense' ],
+	featured_projects: ['dimensions', 'canvaphotoeditor', 'hypertangram'],
 	projects: [
+		{
+			id: 'dimensions',
+			name: 'Dimensions',
+			short_description: 'Generative art and augmented reality.',
+			tech: ['Vue.js', 'three.js'],
+			image: {
+				src: 'img/dimensions_thumb.jpg',
+				alt: 'clip of piece'
+			}
+		},
 		{
 			id: 'canvaphotoeditor',
 			name: 'Canva Photo Editor',
@@ -14,7 +24,7 @@ export default {
 				src: 'img/canvaphotoeditor_thumb.jpg',
 				alt: 'screenshot'
 			},
-			tags: ['web', 'webapp', 'ui', 'reactjs', 'webpack', 'webgl', 'tool'],
+			tags: ['web', 'webapp', 'ui', 'reactjs', 'webpack', 'webgl', 'tool']
 		},
 		{
 			id: 'canvalogomaker',
@@ -26,19 +36,25 @@ export default {
 				src: 'img/canvalogomaker_0.jpg',
 				alt: 'screenshot'
 			},
-			tags: ['web', 'webapp', 'ui', 'animation', 'css', 'tool'],
+			tags: ['web', 'webapp', 'ui', 'animation', 'css', 'tool']
 		},
 		{
 			id: 'canvaenterprise',
 			name: 'Canva Enterprise',
-			links: [{ name: 'Web', url: 'https://web.archive.org/web/20190401230423/https://about.canva.com/enterprise/' }],
+			links: [
+				{
+					name: 'Web',
+					url:
+						'https://web.archive.org/web/20190401230423/https://about.canva.com/enterprise/'
+				}
+			],
 			short_description: 'Page presenting Canva’s Enterprise offering.',
 			tech: ['HTML5', 'SCSS', 'JS'],
 			image: {
 				src: 'img/canvaenterprise_0.jpg',
 				alt: 'screenshot'
 			},
-			tags: ['web', 'website', 'ui', 'animation', 'css', 'canva'],
+			tags: ['web', 'website', 'ui', 'animation', 'css', 'canva']
 		},
 		{
 			id: 'kalabasa',
@@ -49,7 +65,7 @@ export default {
 				src: 'img/kalabasa_0.jpg',
 				alt: 'screenshot of intro page'
 			},
-			tags: ['web', 'website', 'ui', 'rollup', 'js', 'animation'],
+			tags: ['web', 'website', 'ui', 'rollup', 'js', 'animation']
 		},
 		{
 			id: 'heartgame',
@@ -58,89 +74,143 @@ export default {
 			tech: ['HTMl5', 'Javascript'],
 			image: {
 				src: 'img/heartgame.jpg',
-				alt: 'game visual',
+				alt: 'game visual'
 			},
-			tags: ['web', 'game', 'videogame', 'gamedev'],
+			tags: ['web', 'game', 'videogame', 'gamedev']
 		},
 		{
 			id: 'hypertangram',
 			name: 'Hypertangram',
-			links: [{ name: 'Google Play', url: 'https://play.google.com/store/apps/details?id=com.kalabasagames.hypertangram' }],
+			links: [
+				{
+					name: 'Google Play',
+					url:
+						'https://play.google.com/store/apps/details?id=com.kalabasagames.hypertangram'
+				}
+			],
 			short_description: 'Mobile puzzle game about packing shapes together.',
 			tech: ['Android', 'Kotlin', 'Realm', 'Robolectric'],
 			image: {
 				src: 'img/hypertangram_0.jpg',
 				alt: 'screenshot of gameplay'
 			},
-			tags: ['game', 'videogame', 'gamedev', 'design', 'java', 'ux', 'graphics', 'music', 'app'],
+			tags: [
+				'game',
+				'videogame',
+				'gamedev',
+				'design',
+				'java',
+				'ux',
+				'graphics',
+				'music',
+				'app'
+			]
 		},
 		{
 			id: 'arosybrokenalbum',
 			name: 'A Rosy Broken Album',
-			links: [{ name: 'Soundcloud', url: 'https://soundcloud.com/lreaadna/sets/a-rosy-broken-album' }],
+			links: [
+				{
+					name: 'Soundcloud',
+					url: 'https://soundcloud.com/lreaadna/sets/a-rosy-broken-album'
+				}
+			],
 			short_description: 'Solo piano pieces about feelings.',
 			image: {
 				src: 'img/arosybrokenalbum_0.jpg',
-				alt: 'cover art',
+				alt: 'cover art'
 			},
-			tags: ['art', 'music', 'piano', 'solo'],
+			tags: ['art', 'music', 'piano', 'solo']
 		},
 		{
 			id: 'freeformgesturedetector',
 			name: 'FreeformGestureDetector',
 			links: [
-				{ name: 'Github', url: 'https://github.com/Kalabasa/FreeformGestureDetector/' },
-				{ name: 'Android Arsenal', url: 'https://android-arsenal.com/details/1/6939' },
+				{
+					name: 'Github',
+					url: 'https://github.com/Kalabasa/FreeformGestureDetector/'
+				},
+				{
+					name: 'Android Arsenal',
+					url: 'https://android-arsenal.com/details/1/6939'
+				}
 			],
 			short_description: 'Multi-touch transform gesture detector for Android.',
 			tech: ['Android', 'Kotlin'],
 			image: {
 				src: 'img/freeformgesturedetector_head.png',
-				alt: 'illustration',
+				alt: 'illustration'
 			},
-			tags: ['library', 'java', 'android', 'open source', 'hypertangram', 'ui'],
+			tags: ['library', 'java', 'android', 'open source', 'hypertangram', 'ui']
 		},
 		{
 			id: 'planetdefense',
 			name: 'PlanetDefense',
 			links: [{ name: 'Web', url: 'https://kalabasa.github.io/PlanetDefense' }],
-			short_description: 'Game made in 48 hours for Ludum Dare 38: A Small World',
+			short_description:
+				'Game made in 48 hours for Ludum Dare 38: A Small World',
 			full_description: `Web game similar to Missile Command, but it's circular and it's made in 48 hours as entry for the popular game jam Ludum Dare.`,
 			tech: ['Javascript', 'PixiJS'],
 			image: {
 				src: 'img/planetdefense_0.jpg',
 				alt: 'screenshot of gameplay'
 			},
-			tags: ['web', 'game', 'videogame', 'gamedev', 'pixi'],
+			tags: ['web', 'game', 'videogame', 'gamedev', 'pixi']
 		},
 		{
 			id: 'conversations',
 			name: 'Conversations EP',
 			links: [
-				{ name: 'Spotify', url: 'https://open.spotify.com/album/2LEKLuoWYauyAlQ8JwoPc2' },
-				{ name: 'Bandcamp', url: 'http://lreaadna.bandcamp.com/album/conversations' }
+				{
+					name: 'Spotify',
+					url: 'https://open.spotify.com/album/2LEKLuoWYauyAlQ8JwoPc2'
+				},
+				{
+					name: 'Bandcamp',
+					url: 'http://lreaadna.bandcamp.com/album/conversations'
+				}
 			],
 			short_description: 'Chill jazzhop music',
-			full_description: 'Four-piece composition of mellow hip-hop beats, jazz harmony, and mild piano improvisations. Influences range from jazz, funk, and bossa nova to lo-fi, hip-hop, and new-age.',
+			full_description:
+				'Four-piece composition of mellow hip-hop beats, jazz harmony, and mild piano improvisations. Influences range from jazz, funk, and bossa nova to lo-fi, hip-hop, and new-age.',
 			tech: ['LMMS', 'Audacity'],
 			image: {
 				src: 'img/conversations_0.jpg',
 				alt: 'album art'
 			},
-			tags: ['art', 'music', 'jazz', 'jazzhop', 'chillhop'],
+			tags: ['art', 'music', 'jazz', 'jazzhop', 'chillhop']
 		},
 		{
 			id: 'dynastymap',
 			name: 'DynastyMap',
 			short_description: 'Philippine political dynasty visualization system',
-			full_description: 'System that processes election results and visualizes dynasty data. Features choropleth map plus tag cloud overlay for insightful visualization.',
-			tech: ['React', 'Backbone.js', 'Mapbox', 'D3.js', 'Material Design', 'PHP', 'Python'],
+			full_description:
+				'System that processes election results and visualizes dynasty data. Features choropleth map plus tag cloud overlay for insightful visualization.',
+			tech: [
+				'React',
+				'Backbone.js',
+				'Mapbox',
+				'D3.js',
+				'Material Design',
+				'PHP',
+				'Python'
+			],
 			image: {
 				src: 'img/dynastymap_0.jpg',
 				alt: `screenshot of welcome screen`
 			},
-			tags: ['web', 'js', 'reactjs', 'backbonejs', 'mapboxjs', 'leaflet', 'd3js', 'politics', 'webapp'],
-		},
+			tags: [
+				'web',
+				'js',
+				'reactjs',
+				'backbonejs',
+				'mapboxjs',
+				'leaflet',
+				'd3js',
+				'politics',
+				'webapp'
+			]
+		}
 	],
 
 	jobs: [
@@ -149,17 +219,26 @@ export default {
 			company: 'Canva',
 			date: ['2018', 'now'],
 			tech: ['React', 'Node', 'TypeScript'],
-			description: 'Frontend growth engineering. We run experiments and iterate – to empower the world to design with us!',
+			description:
+				'Frontend growth engineering. We run experiments and iterate – to empower the world to design with us!',
 			thumbnail: {
 				src: 'img/thumb-canva.png',
-				alt: 'Canva logo',
-			},
+				alt: 'Canva logo'
+			}
 		},
 		{
 			name: 'Web Developer',
 			company: 'Azeus',
 			date: ['2017', '2018'],
-			tech: ['Vue.js', 'Webpack', 'Spring Boot', 'Gradle', 'TypeScript', 'Java', 'Stylus'],
+			tech: [
+				'Vue.js',
+				'Webpack',
+				'Spring Boot',
+				'Gradle',
+				'TypeScript',
+				'Java',
+				'Stylus'
+			],
 			description: 'Full-stack web and systems development.'
 		},
 		{
@@ -170,39 +249,72 @@ export default {
 			description: `Worked on the Android app for the digital meetings and collaboration system called <a target='_blank' href='https://www.azeusconvene.com/'>Convene</a>.`,
 			thumbnail: {
 				src: 'img/thumb-convene.png',
-				alt: 'Convene logo',
-			},
+				alt: 'Convene logo'
+			}
 		},
 		{
 			name: 'Software Engineering Intern',
 			company: 'Azeus',
 			date: ['2014'],
 			tech: ['Android', 'AndEngine', 'Vert.x', 'Java'],
-			description: 'Worked on a peer-to-peer multiplayer card game on Android that features a disturbing Easter egg.',
+			description:
+				'Worked on a peer-to-peer multiplayer card game on Android that features a disturbing Easter egg.'
 		},
 		{
 			name: 'Flash Developer',
 			alt_company: 'Freelance',
 			date: ['2009'],
-			tech: ['ActionScript 3.0', 'ActionScript 2.0', 'Adobe Flash', 'FlashDevelop', 'Macromedia Flash'],
-			description: 'Created animated, sometimes interactive, Internet advertisements. Indirectly contributed to the rise of ad-blockers.'
+			tech: [
+				'ActionScript 3.0',
+				'ActionScript 2.0',
+				'Adobe Flash',
+				'FlashDevelop',
+				'Macromedia Flash'
+			],
+			description:
+				'Created animated, sometimes interactive, Internet advertisements. Indirectly contributed to the rise of ad-blockers.'
 		},
 		{
 			name: 'Powerpoint “Engineer”',
 			alt_company: 'Freelance',
 			date: ['2009'],
 			tech: ['Photoshop', 'Powerpoint'],
-			description: 'Illustrated, animated, and created <em>interactive</em> biochemistry learning materials for some learning institute. In Powerpoint.'
+			description:
+				'Illustrated, animated, and created <em>interactive</em> biochemistry learning materials for some learning institute. In Powerpoint.'
 		}
 	],
 
 	links: [
-		{ name: 'Email', img: 'img/icon-mail.svg', href: 'mailto:lgmrada@gmail.com' },
-		{ name: 'Github', img: 'img/icon-github.svg', href: 'https://github.com/Kalabasa' },
-		{ name: 'LinkedIn', img: 'img/icon-linkedin.svg', href: 'https://ph.linkedin.com/in/lean-godffrey-rada' },
-		{ name: 'Soundcloud', img: 'img/icon-music.svg', href: 'https://soundcloud.com/lreaadna' },
-		{ name: 'Ludum Dare', img: 'img/icon-game.svg', href: 'https://ldjam.com/users/kalabasa/games' },
+		{
+			name: 'Email',
+			img: 'img/icon-mail.svg',
+			href: 'mailto:lgmrada@gmail.com'
+		},
+		{
+			name: 'Github',
+			img: 'img/icon-github.svg',
+			href: 'https://github.com/Kalabasa'
+		},
+		{
+			name: 'LinkedIn',
+			img: 'img/icon-linkedin.svg',
+			href: 'https://ph.linkedin.com/in/lean-godffrey-rada'
+		},
+		{
+			name: 'Soundcloud',
+			img: 'img/icon-music.svg',
+			href: 'https://soundcloud.com/lreaadna'
+		},
+		{
+			name: 'Ludum Dare',
+			img: 'img/icon-game.svg',
+			href: 'https://ldjam.com/users/kalabasa/games'
+		},
 		// { name: 'Ludum Dare (Legacy)', img: 'img/icon-game.svg', href: 'http://legacy.ludumdare.com/compo/author/kalabasa/' },
-		{ name: 'CodePen', img: 'img/icon-codepen.svg', href: 'https://codepen.io/kalabasa/' },
-	],
+		{
+			name: 'CodePen',
+			img: 'img/icon-codepen.svg',
+			href: 'https://codepen.io/kalabasa/'
+		}
+	]
 };
---
+++ src/pages/works/dimensions2.md
@@ -0,0 +1,148 @@
+<!--{
+	"template": "work",
+	"data": "projects_byid.dimensions"
+}-->
+
+# Dimensions
+
+## Part 2: Augmented reality
+
+<small class="small-block">[Part 1](dimensions.html)</small>
+
+The idea of including augmented reality was wholly inspired by [AR.js](https://github.com/jeromeetienne/AR.js), an awesome project that brings easy and performant augmented reality for the web.
+
+<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/0MtvjFg7tik" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+The planned user flow was like this:
+
+* User sees the piece and notices QR code.
+* User whips out smartphone to scan QR code.
+* Smartphone is directed to the app.
+* App opens the phone’s camera to track the piece.
+* App superimposes virtual art on the physical art.
+
+Implementing step 4 (tracking the piece) was met with difficulty because AR.js required special markers in order to track the 3D scene.
+
+The prints were already finalized, so I couldn’t add AR markers on it then. Plus, the piece already had a QR code slapped on it. Adding any more technical tags would’ve ruined it.
+
+I looked around for alternatives like **Tracking.js**, **OpenCV**, and even **TensorFlow**, but ultimately implemented my own image recognition algorithm.
+
+---
+
+## Image recognition
+
+[**Image recognition**](https://en.wikipedia.org/wiki/Computer_vision#Recognition) is a computer vision problem of determining whether an image contains some specific object or not.
+
+<span class="bleed">
+  <video muted autoplay loop playsinline>
+    <source src="../video/dimensions_imagerec.mp4">
+    <a href="../video/dimensions_imagerec.mp4">Demo video</a>
+  </video>
+</span>
+<span class="caption">The app recognizes when the target piece has been aligned.</span>
+
+There exists many solutions to this problem ranging from simple histogram matching to convolutional neural networks. These days everyone just uses neural networks and deep learning if possible.
+
+These technologies power some apps like Face Swap and Snaphchat filters, and is also used for things like mass camera surveillance.
+
+In my case however, I’ve simplified the problem to determining whether any of the three pieces are in the center frame in the user’s camera, or not. No position tracking. This is just a yes/no problem.
+
+Consequently, my algorithm was very simple:
+
+I start by picking three regions over the camera image.
+
+Then I get the average color of each region.
+
+TODO DEMO
+
+These specific regions were picked so as to obtain the pieces’ key characteristics.
+
+Then the top and middle colors are subtracted, as well as the middle and bottom, producing two colors.
+
+TODO DEMO
+
+The subtraction operation cancels out variances in lighting, camera quality, and white balance. This step can be compared to a convolution in 2D image processing.
+
+TODO Theory behind subtraction?
+PerceivedColor = TrueColor * Light + Noise
+
+The channels of the differences are separated and combined as one series of numbers. This series of numbers is called a feature vector, i.e., numbers that summarize the image.
+
+TODO DEMO
+
+Finally, the vector is normalized.
+
+Turning images into vectors makes the problem of comparing images a mathematical one.
+
+The Euclidean distance between two feature vectors approximate the difference between two corresponding images.
+
+This can be used to compare the camera image with a reference image - a target image to match.
+
+I obtained reference feature vectors from the raw pixel data and on-site photos. On each frame, these reference vectors are compared to the feature vectors processed from the camera. If the vectors match, then the images match.
+
+TODO Live demo with vector matching
+
+Once it gets a match the **augmented reality** experience starts rolling in.
+
+---
+
+## Augmented reality
+
+Remember the concept for the tiles? The pen, pencil, brush, et al?
+
+The vision for the augmented reality part was that these tiles would come alive, burst out of the piece, and start drawing streaks of paint, ink, or whatever in the air.
+
+![AR notes](../img/dimensions_arnotes.jpg)
+
+The AR part was rendered using [three.js](https://threejs.org/). I’ve used three.js before and it was great, very easy-to-learn API, great examples.
+
+I was quickly able to render virtual objects in real space:
+
+<video muted autoplay loop playsinline>
+  <source src="../video/dimensions_ar1.mp4">
+  <a href="../video/dimensions_ar1.mp4">Demo video</a>
+</video>
+
+This was made by simply overlaying a transparent three.js `<canvas>` onto the `<video>` that’s streaming the camera feed.
+
+A three.js extension called `DeviceOrientationControls` provides synchronization between the device’s orientation and the virtual camera.
+
+Note that only device orientation can be tracked. Translations in space weren’t tracked, so virtual objects would appear follow the device. The experience was designed around this limitation.
+
+<small class="small-block">There was a bug on iOS Safari with orientation tracking due to changes in their permissions model (?), which apparently was just released July 2019. The bug was only discovered in production (i.e., at the exhibit) and I sadly witnessed some iPhone users not getting the full experience.</small>
+
+Modeling the tiles as 3D objects were simply extrusions of the tiles’ paths, made easy with three.js’s `ExtrudeGeometry`.
+
+The “paint” trails were made using an old unmaintained library called [**TrailRendererJS**](https://github.com/mkkellogg/TrailRendererJS), which surprisingly still works, although it bugs out when the camera isn’t at the origin.
+
+The floating tiles’ movement behavior were guided by the triangle wave function defined as `arccos(sin(x))`. It looks like this:
+
+<span>![triangle wave function](../img/dimensions_trianglewave.png)
+  <span class="caption">`y = arccos(sin(x))`</span>
+</span>
+
+Multiplying the sine value smoothens out the curve which makes the tiles’ movement more organic.
+
+<span>![smooth triangle wave function](../img/dimensions_trianglewave2.png)
+  <span class="caption">`y = arccos(0.95 sin(x))`</span>
+</span>
+
+Imagine that path multplied for each floating tile.
+
+![many smooth triangle wave function](../img/dimensions_waves.png)
+
+Then the paths are wrapped around an imaginary cylinder around the user’s position. A little bit of randomness added.
+
+resulting in an organized chaos of particles orbiting the user, and creating criss-crossing trails.
+
+<video muted autoplay loop playsinline>
+  <source src="../video/dimensions_trail.mp4">
+  <a href="../video/dimensions_trail.mp4">Demo video</a>
+</video>
+
+A fun experiment is when the paint trails are allowed to go on indefinitely. These trails would eventually produce another piece of 2D generative art.
+
+<span>![](../img/dimensions_trailart.png)
+  <span class="caption">Trail art</span>
+</span>
+
