7ba194df76aa2409ee13a92b80289d6462741d68
Lean Rada <Kalabasa@users.noreply.github.com>
2022-02-08 18:17:10 +0800

    Formatting home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,4 +4,23 @@ if (ENV_DEBUG) console.log('index.js execute');
 
 page.ready(() => {
 	if (ENV_DEBUG) console.log('index.js ready');
+
+	const introSection = [...document.querySelectorAll('.intro')].pop();
+	introSection.addEventListener('click', (event) => {
+		console.log(event.target);
+		if (
+			event.target instanceof HTMLAnchorElement &&
+			event.target.classList.contains('intro-anchor-link') &&
+			event.target.hash
+		) {
+			const target = document.querySelector(event.target.hash);
+			if (!target) return;
+
+			target.scrollIntoView({
+				behavior: 'smooth',
+			});
+			event.preventDefault();
+			console.log('auy');
+		}
+	});
 });
