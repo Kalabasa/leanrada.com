019573790db3270f6121e7029131eb34ff43fe22
Kalabasa <Kalabasa@users.noreply.github.com>
2023-02-06 21:04:36 +1100

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -e
+
+git status --porcelain
+
+# Build project
+git branch -f build
+git checkout build
+
+yarn install
+yarn clean
+yarn build
+
+git add -f "docs/"
+git commit -m "Deploy"
+
+# Update prod. master == prod
+git checkout -f master
+
+rm -rf node_modules/
+git rm -rf .
+git clean -df
+
+git checkout -f build -- "docs/"
+
+touch .nojekyll
+git add .nojekyll
+
+git commit -m "Deploy"
+git diff HEAD~
+echo "Run `git push` to deploy"
