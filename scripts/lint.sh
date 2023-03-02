e9953c45dab2319a86a4ec978af44e6289f8b125
Kalabasa <Kalabasa@users.noreply.github.com>
2023-03-03 01:53:35 +1100

    Lint

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,5 +4,10 @@ set -e
 cd "$(dirname "$0")"/..
 pwd
 
+touch out/lint.txt
+truncate -s 0 out/lint.txt
+
 yarn build-dev > /dev/null
-npx textlint -o out/lint.txt --no-color -f pretty-error "out/site/wares/*/**/*.html" "out/site/blog/*/**/*.html"
+
+npx textlint -o out/lint.txt --no-color -f pretty-error "out/site/wares/*/**/*.html" "out/site/blog/*/**/*.
+html"
