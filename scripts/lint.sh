f58713a318a6cecd0dcb6ec1b84e4aa2744c3087
Kalabasa <Kalabasa@users.noreply.github.com>
2023-06-26 19:00:07 +1000

    Fix typo

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -10,7 +10,7 @@ truncate -s 0 out/textlint.tmp
 npx textlint -o out/textlint.tmp -f pretty-error \
   "out/site/wares/*/**/*.html" \
   "out/site/notes/*/**/*.html" \
-  "out/site/misec/ide-reviews/index.html"
+  "out/site/misc/ide-reviews/index.html"
 
 truncate -s 0 out/eslint.tmp
 ! npx eslint -o out/eslint.tmp "out/site/**/*.html"
