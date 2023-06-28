3105560c9152fc15a5062f76c0a4c96c714c250f
Kalabasa <Kalabasa@users.noreply.github.com>
2023-06-28 20:07:46 +0000

    Deploy

---
+++ docs/scripts/main-footer-main-header.js
@@ -0,0 +1,146 @@
+
+  // subtle smooth scroll
+  (() => {
+    let top = 0;
+
+    const topBtn = document.querySelector(".main-footer-top-btn");
+    topBtn.addEventListener("click", (event) => {
+      event.preventDefault();
+      top = 200;
+      loop();
+    });
+
+    function loop() {
+      top *= 0.6;
+      window.scrollTo(0, top);
+      if (top < 1) {
+        window.scrollTo(0, 0);
+      } else {
+        requestAnimationFrame(loop);
+      }
+    }
+  })();
+
+
+  (() => {
+    const mainHeader = document.querySelector(".main-header");
+    const indicator = document.querySelector(".main-header-indicator");
+
+    let currentY = 0;
+    let currentYTarget = 0;
+
+    let mouseHovering = false;
+
+    let isTouch = false;
+    let lastScrollY = window.scrollY;
+    let lastCursorY = 0;
+    window.addEventListener("scroll", debounce(onScroll));
+    window.addEventListener("mousemove", debounce(onMouseMove, 100));
+    window.addEventListener("touchstart", onWindowTouchStart);
+    window.addEventListener("touchmove", onWindowTouchMove);
+    mainHeader.addEventListener("touchstart", onTouchStart);
+
+    function onScroll(event) {
+      const dy = window.scrollY - lastScrollY;
+
+      // move with the page
+      currentY -= dy;
+      if (currentY > 0) {
+        currentY = 0;
+      } else if (currentY < -mainHeader.offsetHeight) {
+        currentY = -mainHeader.offsetHeight;
+      }
+
+      currentYTarget = currentY;
+      updateDOM();
+
+      lastScrollY = window.scrollY;
+    }
+
+    function onMouseMove(event) {
+      if (isTouch) return;
+
+      const dy = event.clientY - lastCursorY;
+
+      // show when mouse goes near the top
+      const scaledDy = Math.sign(dy) * Math.log1p(Math.abs(dy)) * 20;
+      if (dy < 0 && event.clientY + scaledDy < mainHeader.offsetHeight) {
+        currentYTarget = 0;
+        mouseHovering = true;
+      } else if (
+        mouseHovering &&
+        dy > 0 &&
+        event.clientY > mainHeader.offsetHeight * 4
+      ) {
+        currentYTarget = Math.max(-window.scrollY, -mainHeader.offsetHeight);
+        mouseHovering = false;
+      }
+
+      updateDOM();
+      lastCursorY = event.clientY;
+    }
+
+    function onWindowTouchStart(event) {
+      isTouch = true;
+      lastCursorY = event.touches[0].clientY;
+    }
+    function onWindowTouchMove(event) {
+      const dy = event.touches[0].clientY - lastCursorY;
+
+      // move with touch but only if at edge
+      if (window.scrollY <= 0 && dy > 0) {
+        currentY += dy;
+        if (currentY >= 0) {
+          currentY = 0;
+          document.body.style.overscrollBehaviorY = null;
+        } else if (currentY < 0) {
+          document.body.style.overscrollBehaviorY = "none";
+        }
+
+        currentYTarget = currentY;
+        updateDOM();
+      }
+
+      lastCursorY = event.touches[0].clientY;
+    }
+
+    function onTouchStart(event) {
+      currentYTarget = 0;
+      updateDOM();
+      console.log("onTouchStart");
+    }
+
+    const updateDOM = debounce(() => {
+      mainHeader.style.transform = `translateY(${currentY.toFixed(2)}px)`;
+
+      const isHidden = currentY < -mainHeader.offsetHeight * 0.8;
+      mainHeader.classList.toggle("hidden", isHidden);
+
+      if (Math.abs(currentY - currentYTarget) > 1) {
+        currentY += (currentYTarget - currentY) * 0.2;
+        requestAnimationFrame(updateDOM);
+      } else {
+        currentY = currentYTarget;
+      }
+    });
+
+    if (mainHeader.classList.contains("prehide")) {
+      document.body.style.overscrollBehaviorY = "none";
+      currentY = currentYTarget = -mainHeader.offsetHeight;
+      updateDOM();
+    }
+
+    function debounce(fn, ms = 0) {
+      let recentlyFired = false;
+      return (...args) => {
+        if (recentlyFired) return;
+        recentlyFired = true;
+        if (ms === 0) {
+          requestAnimationFrame(() => (recentlyFired = false));
+        } else {
+          setTimeout(() => (recentlyFired = false), ms);
+        }
+        return fn(...args);
+      };
+    }
+  })();
