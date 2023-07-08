88f960d8d4afb8e7e626c28540069c37b2a89a80
Kalabasa <Kalabasa@users.noreply.github.com>
2023-07-09 00:03:41 +1000

    guestbook fake data

--- src/guestbook/worker.ts
+++ src/guestbook/worker.ts
@@ -25,16 +25,75 @@ export interface Env {
   // MY_QUEUE: Queue;
 }
 
-export default {
-  async fetch(
-    request: Request,
-    env: Env,
-    ctx: ExecutionContext
-  ): Promise<Response> {
-    return new Response('{ "h": "ello", "w": "orld" }', {
+// Must be constant forever
+const PAGE_WIDTH = 20;
+const PAGE_HEIGHT = 10;
+
+type Page = {
+  content: string;
+  edits: Edit[];
+};
+
+type Edit = {
+  user: string;
+  inserts: Insert[];
+};
+
+type Insert = {
+  page: number;
+  offset: number;
+  text: string;
+};
+
+async function fetch(
+  request: Request,
+  env: Env,
+  ctx: ExecutionContext
+): Promise<Response> {
+  try {
+    const url = new URL(request.url);
+
+    let pageIndices;
+    pageIndices = parsePageIndices(url);
+    if (pageIndices.length > 2) pageIndices.length = 2;
+
+    const pageContents = await Promise.all(
+      pageIndices.map((i) => getPageContent(i))
+    );
+
+    return new Response(JSON.stringify(pageContents), {
       headers: {
         "content-type": "application/json;charset=UTF-8",
       },
     });
-  },
-};
+  } catch (e) {
+    console.error(e);
+    return new Response(null, { status: 404 });
+  }
+}
+
+function parsePageIndices(url: URL): number[] {
+  const strPages = url.searchParams.getAll("page");
+  const numPages = strPages.map((s) => Number.parseInt(s, 10));
+
+  if (numPages.some((p) => Number.isNaN(p))) {
+    throw new TypeError("NaN page parameter");
+  }
+
+  return numPages;
+}
+
+async function getPageContent(index: number): Promise<string> {
+  return `12345678901234567890
+12345678901234567890
+12345678901234567890
+12345678901234567890
+12345678901234567890
+12345678901234567890
+12345678901234567890
+12345678901234567890
+12345678901234567890
+12345678901234567890`;
+}
+
+export default { fetch };
