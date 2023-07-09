f49f41c3436303c6031f275e3bba5898eab19006
Kalabasa <Kalabasa@users.noreply.github.com>
2023-07-09 21:27:48 +1000

    WIP updates

--- src/guestbook/worker.ts
+++ src/guestbook/worker.ts
@@ -1,99 +1,58 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-  //
-  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-  // MY_SERVICE: Fetcher;
-  //
-  // Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-  // MY_QUEUE: Queue;
+  GITHUB_SECRET: string;
 }
 
 // Must be constant forever
-const PAGE_WIDTH = 20;
-const PAGE_HEIGHT = 10;
+const PAGE_WIDTH = 40;
+const PAGE_HEIGHT = 30;
 
-type Page = {
-  content: string;
-  edits: Edit[];
-};
+const sourceDataURL =
+  "https://raw.githubusercontent.com/Kalabasa/kalabasa.github.io/src/src/site/guestbook/data.json";
 
-type Edit = {
-  user: string;
-  inserts: Insert[];
-};
-
-type Insert = {
+type SubmitRequest = {
   page: number;
-  offset: number;
-  text: string;
+  content: string;
 };
 
-async function fetch(
+async function handleRequest(
   request: Request,
   env: Env,
   ctx: ExecutionContext
 ): Promise<Response> {
   try {
-    const url = new URL(request.url);
+    const submitRequest: Partial<SubmitRequest> = await request.json();
+    checkSubmitRequest(submitRequest);
+    console.log(submitRequest);
 
-    let pageIndices;
-    pageIndices = parsePageIndices(url);
-    if (pageIndices.length > 2) pageIndices.length = 2;
+    const sourceData = await fetch(sourceDataURL);
+    console.log(sourceData);
 
-    const pageContents = await Promise.all(
-      pageIndices.map((i) => getPageContent(i))
-    );
-
-    return new Response(JSON.stringify(pageContents), {
-      headers: {
-        "content-type": "application/json;charset=UTF-8",
-      },
-    });
+    return new Response(null);
   } catch (e) {
-    console.error(e);
+    console.error(
+      "Returning 404 due to",
+      e instanceof Error ? e.name : " unknown error"
+    );
     return new Response(null, { status: 404 });
   }
 }
 
-function parsePageIndices(url: URL): number[] {
-  const strPages = url.searchParams.getAll("page");
-  const numPages = strPages.map((s) => Number.parseInt(s, 10));
-
-  if (numPages.some((p) => Number.isNaN(p))) {
-    throw new TypeError("NaN page parameter");
-  }
-
-  return numPages;
+function checkSubmitRequest(
+  submitRequest: Partial<SubmitRequest>
+): asserts submitRequest is SubmitRequest {
+  if (typeof submitRequest.page !== "number")
+    throw new TypeError("submitRequest is missing page");
+  if (Math.floor(submitRequest.page) !== submitRequest.page)
+    throw new TypeError("submitRequest.page is not an integer");
+  if (typeof submitRequest.content !== "string")
+    throw new TypeError("submitRequest is missing content");
+  if (
+    submitRequest.content.length !==
+    PAGE_WIDTH * PAGE_HEIGHT + (PAGE_HEIGHT - 1)
+  )
+    throw new TypeError("submitRequest.content is malformed");
+  if (submitRequest.content.split("\n").length !== PAGE_HEIGHT)
+    throw new TypeError("submitRequest.content is malformed");
 }
 
-async function getPageContent(index: number): Promise<string> {
-  return `12345678901234567890
-12345678901234567890
-12345678901234567890
-12345678901234567890
-12345678901234567890
-12345678901234567890
-12345678901234567890
-12345678901234567890
-12345678901234567890
-12345678901234567890`;
-}
-
-export default { fetch };
+export default { fetch: handleRequest };
