c26124be09019f568e8f82d4c70cc4e070b2c7ea
Kalabasa <Kalabasa@users.noreply.github.com>
2023-11-30 22:07:54 +0000

    Deploy

---
+++ docs/scripts/lily58.js
@@ -0,0 +1,21 @@
+
+  (() => {
+    const lilies = document.querySelectorAll(".lily58");
+    for (const lily of lilies) {
+      const layerButtons = lily.querySelectorAll(".lily58-layer-button");
+
+      if (!layerButtons.length) continue;
+
+      let currentLayer = 0;
+      lily.classList.add("lily58-on-layer" + currentLayer);
+      const toggleLayer = () => {
+        lily.classList.remove("lily58-on-layer" + currentLayer);
+        currentLayer = 1 - currentLayer;
+        lily.classList.add("lily58-on-layer" + currentLayer);
+      };
+
+      for (const layerButton of layerButtons) {
+        layerButton.addEventListener("click", toggleLayer);
+      }
+    }
+  })();
