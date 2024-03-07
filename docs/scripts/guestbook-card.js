8a933b2ec13cc21fdbc5e7fe01c5e076633c0650
Kalabasa <Kalabasa@users.noreply.github.com>
2024-03-07 15:55:57 +0000

    Deploy

--- docs/scripts/guestbook-card.js
+++ docs/scripts/guestbook-card.js
@@ -1,6 +1,8 @@
 
   import { BaseElement } from "/lib/base_element.mjs";
 
+  const MAX_STAMPS = 4;
+
   customElements.define(
     "guestbook-card-client",
     class GuestbookCard extends BaseElement {
@@ -20,25 +22,29 @@
         const dataAttr = this.getAttribute("data");
         const data = dataAttr ? JSON.parse(dataAttr) : null;
 
-        this.innerHTML = `        
-<textarea
-  class="guestbook-card-body"
-  name="text"
-  placeholder="${disabled ? "" : "Your message"}"
-  ${disabled ? "disabled" : ""}
-  required
-></textarea>
-<input
-  class="guestbook-card-name"
-  name="name"
-  type="text"
-  placeholder="${disabled ? "" : "Name / pseudonym"}"
-  ${disabled ? "disabled" : ""}
-/>
-`;
+        this.innerHTML = `
+  <textarea
+    class="guestbook-card-body"
+    name="text"
+    placeholder="${disabled ? "" : "Your message"}"
+    ${disabled ? "disabled" : ""}
+    required
+  ></textarea>
+  <input
+    class="guestbook-card-name"
+    name="name"
+    type="text"
+    placeholder="${disabled ? "" : "Name / pseudonym"}"
+    ${disabled ? "disabled" : ""}
+  />
+  <div class="guestbook-card-stamp-container"></div>
+  `;
 
         this.bodyInput = this.asyncQuerySelector(".guestbook-card-body");
         this.nameInput = this.asyncQuerySelector(".guestbook-card-name");
+        this.stampContainer = this.asyncQuerySelector(
+          ".guestbook-card-stamp-container"
+        );
 
         if (data?.text) {
           this.bodyInput.promise.then((input) => (input.value = data.text));
@@ -47,9 +53,39 @@
           this.nameInput.promise.then((input) => (input.value = data.name));
         }
 
+        for (const stamp of data?.stamps ?? []) {
+          this.addStamp(stamp.typeIndex, stamp.x, stamp.y);
+        }
+
         const { getDefaults, getCSS } = await loadGuestbookCardLib();
         this.currentStyle = data?.style ?? getDefaults();
         this.setAttribute("style", getCSS(this.currentStyle));
+      }
+
+      async addStamp(typeIndex, x, y) {
+        const [stampContainer, { getStampContent }] = await Promise.all([
+          this.stampContainer.promise,
+          loadGuestbookCardLib(),
+        ]);
+
+        const content = getStampContent(typeIndex);
+
+        const stampElement = document.createElement("div");
+        stampElement.innerHTML =
+          content +
+          `<input name="stampTypes" type="hidden" value="${typeIndex}">` +
+          `<input name="stampXs" type="hidden" value="${x}">` +
+          `<input name="stampYs" type="hidden" value="${y}">`;
+
+        stampElement.classList.add("guestbook-card-stamp");
+
+        stampElement.style.left = `${x}%`;
+        stampElement.style.top = `${y}%`;
+
+        stampContainer.appendChild(stampElement);
+        while (stampContainer.childElementCount > MAX_STAMPS) {
+          stampContainer.firstElementChild.remove();
+        }
       }
 
       async updateStyle(style) {
