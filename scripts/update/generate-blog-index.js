f9b8da00cf0468fd375ef5c0b1634765350c6370
Kalabasa <Kalabasa@users.noreply.github.com>
2024-03-24 22:56:34 +1100

    Update featured

--- scripts/update/generate-blog-index.js
+++ scripts/update/generate-blog-index.js
@@ -64,7 +64,7 @@ async function main() {
       const title = pName.text();
       if (!title) throw new Error("Missing title!");
 
-      const media = uMedia.attr("src");
+      const media = uMedia.attr("src") || uMedia.children("source[src]").first().attr("src");
 
       const date = dtPublished.text();
       if (!date) console.error("No date for page:", title);
