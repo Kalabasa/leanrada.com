522a3a7be7a088ce30510ed5c8b7d5a88c7a83f2
Kalabasa <Kalabasa@users.noreply.github.com>
2024-09-21 01:01:37 +1000

    layout parts

--- guhitkudlit2/site/index.js
+++ guhitkudlit2/site/index.js
@@ -20,6 +20,49 @@ export function Index() {
         height: 100vh;
         overflow: hidden;
       }
+      .appLogo {
+        grid-area: canvas;
+        position: relative;
+        left: calc(var(--size-s) * -1);
+        top: calc(var(--size-s) * -1);
+        justify-self: start;
+        z-index: 1;
+      }
+      /* todo: Logo component */
+      .appLogo > h1 {
+        margin: 0;
+        padding: 0.2em;
+        width: 3.82em;
+        border-radius: 0.09em;
+        font-family: "Cubao Regular", sans-serif;
+        font-size: 1.6em;
+        line-height: 0.7em;
+        font-weight: normal;
+        background: #000;
+      }
+      /* todo: Logo component */
+      .appLogo > h1 > div {
+        background-image: linear-gradient(
+          var(--color-green),
+          var(--color-green) 35%,
+          var(--color-bg) 35%,
+          var(--color-bg) 55%,
+          var(--color-orange) 55%,
+          var(--color-orange)
+        );
+        color: transparent;
+        background-clip: text;
+      }
+      /* todo: Logo component */
+      .appLogo > h1 > div::first-line {
+        font-family: "Cubao Wide", sans-serif;
+        font-size: 0.84em;
+      }
+      .appMenu {
+        grid-area: canvas;
+        justify-self: end;
+        z-index: 2;
+      }
       .appCanvas {
         grid-area: canvas;
       }
@@ -34,16 +77,23 @@ export function Index() {
       }
     </style>
     <div class="app appDesktopLayout">
-      <div class="appCanvas">canvas</div>
-      <div class="appInputPanelArea">
+      <div class="appLogo">
+        ${/* todo: Logo component */ ""}
+        <h1>
+          <div>Guhit<br />Kudlit</div>
+        </h1>
+      </div>
+      <nav class="appMenu">menu</nav>
+      <main class="appCanvas">canvas</main>
+      <aside class="appInputPanelArea">
         <${AppPanel} title=${html`<h2>Text</h2>`}>text<//>
-      </div>
-      <div class="appStylePanelArea">
+      </aside>
+      <aside class="appStylePanelArea">
         <${AppPanel} title=${html`<h2>Style</h2>`}>style<//>
-      </div>
-      <div class="appActionPanelArea">
+      </aside>
+      <aside class="appActionPanelArea">
         <${AppPanel} title=${html`<h2>File</h2>`}>file<//>
-      </div>
+      </aside>
     </div>
   `;
 }
