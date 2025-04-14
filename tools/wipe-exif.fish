498cca424fc5dfc73d1db1afc37c7ce807a28ba5
example@example.com <example@example.com>
2025-04-15 02:47:27 +1000

    wipe

--- tools/wipe-exif.fish
+++ tools/wipe-exif.fish
@@ -1,17 +1,17 @@
 #!/usr/bin/env fish
 
-set -l allowed "Orientation ImageWidth ImageHeight XResolution YResolution ResolutionUnit ColorSpace BitsPerSample Compression PhotometricInterpretation SamplesPerPixel ExifVersion Copyright"
+set -l allowed "Orientation ImageWidth ImageHeight ThumnailOffset ThumbnailLength XResolution YResolution ResolutionUnit ColorSpace BitsPerSample Compression PhotometricInterpretation SamplesPerPixel ExifVersion SubfileType Copyright"
 
 find $argv -type f \
   -not -path "*/node_modules/*" \
   -not -path "*/.git/*" \
   -exec sh -c '
     for file do
-      meta=$(exiftool -s -EXIF:all "$file" | sed -e "s/^\([[:alnum:]]*\).*/\1/g" | grep -vE "$(echo '$allowed' | tr " " "|")")
+      meta=$(exiftool -s -EXIF:all -XMP:all "$file" | sed -e "s/^\([[:alnum:]]*\).*/\1/g" | grep -vE "$(echo '$allowed' | tr " " "|")")
       if [ -n "$meta" ]; then
           wipe_params=$(echo $meta | sed "s/\([[:alnum:]]*\)/-\1=/g" | tr "\n" " ")
           echo exiftool $wipe_params -overwrite_original "$file"
-          exiftool $wipe_params -overwrite_original "$file"
+          exiftool $wipe_params -XMP:all= -overwrite_original "$file"
       fi
     done
   ' bash \{\} +
