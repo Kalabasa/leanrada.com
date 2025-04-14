beb507413a4c74447a4ec2c64d46c3a3036e5e4b
example@example.com <example@example.com>
2025-04-15 02:30:43 +1000

    real

--- tools/wipe-exif.fish
+++ tools/wipe-exif.fish
@@ -2,7 +2,7 @@
 
 set -l allowed "Orientation ImageWidth ImageHeight XResolution YResolution ResolutionUnit ColorSpace BitsPerSample Compression PhotometricInterpretation SamplesPerPixel ExifVersion Copyright"
 
-find main/site/wares/dimensions -type f \
+find $argv -type f \
   -not -path "*/node_modules/*" \
   -not -path "*/.git/*" \
   -exec sh -c '
@@ -11,6 +11,7 @@ find main/site/wares/dimensions -type f \
       if [ -n "$meta" ]; then
           wipe_params=$(echo $meta | sed "s/\([[:alnum:]]*\)/-\1=/g" | tr "\n" " ")
           echo exiftool $wipe_params -overwrite_original "$file"
+          exiftool $wipe_params -overwrite_original "$file"
       fi
     done
   ' bash \{\} +
