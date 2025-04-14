c1c4c60fb87331a8503d777dc5c7c1459fc78a26
example@example.com <example@example.com>
2025-04-15 02:29:48 +1000

    wipe exif script

---
+++ tools/wipe-exif.fish
@@ -0,0 +1,16 @@
+#!/usr/bin/env fish
+
+set -l allowed "Orientation ImageWidth ImageHeight XResolution YResolution ResolutionUnit ColorSpace BitsPerSample Compression PhotometricInterpretation SamplesPerPixel ExifVersion Copyright"
+
+find main/site/wares/dimensions -type f \
+  -not -path "*/node_modules/*" \
+  -not -path "*/.git/*" \
+  -exec sh -c '
+    for file do
+      meta=$(exiftool -EXIF:all "$file" | sed -e "s/^\([^:]*\):.*/\1/g" -e "s/[[:blank:]]//g" | grep -vE "$(echo '$allowed' | tr " " "|")")
+      if [ -n "$meta" ]; then
+          wipe_params=$(echo $meta | sed "s/\([[:alnum:]]*\)/-\1=/g" | tr "\n" " ")
+          echo exiftool $wipe_params -overwrite_original "$file"
+      fi
+    done
+  ' bash \{\} +
