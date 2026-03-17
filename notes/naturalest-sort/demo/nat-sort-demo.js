37b008c55295ace5bec05de504d3abb07f0b0be3
Kalabasa <Kalabasa@users.noreply.github.com>
2026-03-17 20:10:26 +1100

    Deploy main

--- notes/naturalest-sort/demo/nat-sort-demo.js
+++ notes/naturalest-sort/demo/nat-sort-demo.js
@@ -1,4 +1,6 @@
 (() => {
+  import("./data.js").then(({ preload }) => setTimeout(() => preload(), 5_000));
+
   customElements.define(
     "nat-sort-demo",
     class NatSortDemo extends HTMLElement {
@@ -10,9 +12,6 @@
           observer.disconnect();
           this.#run();
         }).observe(this);
-
-        const { preload } = import("./data.js");
-        setTimeout(() => preload(), 5_000);
 
         this.#children = this.innerHTML;
         this.innerHTML = "<i>Loading word embeddings (25MB)&hellip;</i>";
