f12e3bc65d1f6d25da31463f31441d9311514e31
example@example.com <example@example.com>
2026-04-20 22:12:09 +1000

    lint

--- gamegame/eslint.config.js
+++ gamegame/eslint.config.js
@@ -4,4 +4,5 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["tests/browser/**/*.js"], languageOptions: { globals: { ...globals.browser, ...globals.mocha } } },
 ]);
